baddd3209d203d06a4a30dd63dd71fcbf200e19b
Becasuse-hung <4a33a006@stust.edu.tw>
2022-06-20 00:22:51 +0800

    table r

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,81 +1,115 @@
 <template>
   <div class="home">
- <el-tabs  v-model="activeName"
-    type="card"
-    class="demo-tabs"
-    @tab-click="handleClick">
-    <el-tab-pane label="User" name="first">User</el-tab-pane>
-  </el-tabs>
+    <el-tabs
+      v-model="activeName"
+      type="card"
+      class="demo-tabs"
+      @tab-click="handleClick"
+    >
+      <el-tab-pane label="User" name="first">User</el-tab-pane>
+    </el-tabs>
     <header>
       <div class="inputItem">
         <h2>時間</h2>
         <div class="time">
           <el-date-picker
-        v-model="requestData.StartTime"
-        placeholder="年 / 月 / 日"
-        :style="{'max-width': '150px', height:'40px'}"
-      />
-      <div class="lineBox"><span></span></div>
-        <el-date-picker
-        v-model="requestData.EndTime"
-        type="date"
-        placeholder="年 / 月 / 日"
-        size='big'
-        :style="{'max-width': '150px', height:'40px'}"
-      />
-      </div>
+            v-model="requestData.StartTime"
+            placeholder="年 / 月 / 日"
+            :style="{ 'max-width': '150px', height: '40px' }"
+          />
+          <div class="lineBox"><span></span></div>
+          <el-date-picker
+            v-model="requestData.EndTime"
+            type="date"
+            placeholder="年 / 月 / 日"
+            size="big"
+            :style="{ 'max-width': '150px', height: '40px' }"
+          />
+        </div>
       </div>
       <div class="inputItem">
         <h2>動作</h2>
-        <el-select
-          v-model="requestData.SystemActionType"
-          size='big'
-        >
+        <el-select v-model="requestData.SystemActionType" size="big">
           <el-option label="全部" :value="-1" />
           <el-option label="新增" :value="0" />
           <el-option label="修改" :value="1" />
           <el-option label="刪除" :value="2" />
         </el-select>
       </div>
-       <div class="inputItem">
+      <div class="inputItem">
         <h2>內容關鍵字</h2>
         <el-input v-model="requestData.IP" size="big" placeholder="精準搜尋" />
       </div>
-       <div class="inputItem">
+      <div class="inputItem">
         <h2>IP</h2>
-        <el-input class="en" v-model="requestData.Content" placeholder="模糊搜尋" style="max-width:150px" />
+        <el-input
+          class="en"
+          v-model="requestData.Content"
+          placeholder="模糊搜尋"
+          style="max-width: 150px"
+        />
       </div>
-       <div class="inputItem">
+      <div class="inputItem">
         <h2>後台帳號</h2>
         <div>
-        <el-input v-model="requestData.Account" placeholder="模糊搜尋" style="max-width:150px" />
-       <el-button @click="getApi()" :icon="el-icon-search" class="btn" type="primary"></el-button>
-      </div>
+          <el-input
+            v-model="requestData.Account"
+            placeholder="模糊搜尋"
+            style="max-width: 150px"
+          />
+          <el-button
+            @click="getApi()"
+            :icon="el - icon - search"
+            class="btn"
+            type="primary"
+          ></el-button>
+        </div>
       </div>
     </header>
     <div class="container">
-    <el-table class="tabletr" :data="fakeData.column" border :header-cell-style="{background: 'lightgray',borderColor: 'black', 'text-align': 'center'}" :cell-style="{borderColor: 'black'} " :style="{ width:'100%', 'font-size': '16px'}">
-      <el-table-column  v-for="(col, index) in cols"
-        :key="index" :prop="col.prop"
-        :label="col.label" :min-width="col.width" :align="col.align">
-      <template v-slot="slotProps">
-        <p v-if='slotProps.row[col.prop]' :row='slotProps.row'  v-html='slotProps.row[col.prop]' ></p>
-        <p v-else>-</p>
-      </template>
-      </el-table-column>
-  </el-table>
-  </div>
-  <div class="pag">
-   <el-pagination
-      v-model:currentPage="requestData.PageIndex"
-      :page-sizes="[10, 20, 30]"
-      layout=" sizes, prev, pager, next"
-      :total="totalCount"
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-    />
-    <h2>共{{ totalCount }}條</h2>
-  </div>
+      <el-table
+        class="tabletr"
+        :data="fakeData.column"
+        border
+        :header-cell-style="{
+          background: 'lightgray',
+          borderColor: 'black',
+          'text-align': 'center',
+          padding: '20px'
+        }"
+        :cell-style="{ borderColor: 'black' }"
+        :style="{ 'min-width': '100%', 'font-size': '16px' }"
+      >
+        <el-table-column
+          v-for="(col, index) in cols"
+          :key="index"
+          :prop="col.prop"
+          :label="col.label"
+          :min-width="col.width"
+          :align="col.align"
+        >
+          <template v-slot="slotProps">
+            <p
+              v-if="slotProps.row[col.prop]"
+              :row="slotProps.row"
+              v-html="slotProps.row[col.prop]"
+            ></p>
+            <p v-else>-</p>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <div class="pag">
+      <el-pagination
+        v-model:currentPage="requestData.PageIndex"
+        :page-sizes="[10, 20, 30]"
+        layout=" sizes, prev, pager, next"
+        :total="totalCount"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+      <h2>共{{ totalCount }}條</h2>
+    </div>
   </div>
 </template>
 
@@ -100,22 +134,21 @@ export default {
     })
     const totalCount = ref(0)
     const cols = reactive([
-      { prop: 'id', label: '序', width: '5%', align: 'center' },
-      { prop: 'knowledge', label: '調整項目', width: '20%', align: 'center' },
-      { prop: 'operate', label: '動作', width: '10%', align: 'center' },
-      { prop: 'content', label: '內容', width: '25%', align: 'left' },
-      { prop: 'account', label: '操作者', width: '20%', align: 'center' },
-      { prop: 'time', label: '時間', width: '10%', align: 'center' },
-      { prop: 'ip', label: 'IP', width: '10%', align: 'center' }
+      { prop: 'id', label: '序', width: '100px', align: 'center' },
+      { prop: 'knowledge', label: '調整項目', width: '100px', align: 'center' },
+      { prop: 'operate', label: '動作', width: '100px', align: 'center' },
+      { prop: 'content', label: '內容', width: '100px', align: 'left' },
+      { prop: 'account', label: '操作者', width: '100px', align: 'center' },
+      { prop: 'time', label: '時間', width: '100px', align: 'center' },
+      { prop: 'ip', label: 'IP', width: '100px', align: 'center' }
     ])
     const fakeData = reactive({
-      column: [
-      ]
+      column: []
     })
     const pagLeng = ref(0) // total 長
     function getApi () {
       console.log('testData', requestData)
-      fetchFakeApi().then(res => {
+      fetchFakeApi().then((res) => {
         console.log(res.data.infos)
         fakeData.column = res.data.infos
         fakeData.column.reverse()
@@ -125,7 +158,7 @@ export default {
     }
 
     function getApi2 (num) {
-      fetchFakeApi().then(res => {
+      fetchFakeApi().then((res) => {
         console.log(res.data.infos)
         const pageInt = requestData.PageIndex
         const totalC = num * pageInt
@@ -167,11 +200,10 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.home{
+.home {
   padding: 60px;
-
 }
-header{
+header {
   padding: 30px;
   background: lightgray;
   display: flex;
@@ -179,68 +211,68 @@ header{
   align-items: center;
   margin-bottom: 20px;
 
-  .inputItem{
+  .inputItem {
     margin-right: 60px;
-    .time{
-    display: flex;
-    flex-wrap: wrap;
-  }
-    h2{
+    .time {
+      display: flex;
+      flex-wrap: wrap;
+    }
+    h2 {
       text-align: left;
       margin: 5px 0px;
     }
-    .btn{
+    .btn {
       margin-left: 10px;
       transform: translateY(-2px);
     }
-    .en{
+    .en {
       transform: translateY(2px);
     }
   }
-  .lineBox{
+  .lineBox {
     display: flex;
     align-items: center;
   }
-  span{
+  span {
     display: inline-block;
     width: 10px;
     height: 3px;
     opacity: 0.7;
     background-color: gray;
     margin: 0px 5px;
-    }
+  }
   // .btn{
   // align-self: end;
   // }
 }
-.tabletr{
-  border:1px solid black;
-
+.tabletr {
+  border: 1px solid black;
 }
 
-.container{
+.container {
   margin-top: 20px;
+  // overflow-x: auto; ele ui 原本就有
 }
-::v-deep .appd{
-  color:red;
+::v-deep .appd {
+  color: red;
 }
-::v-deep .appX{
-  color:blue;
+::v-deep .appX {
+  color: blue;
 }
-.pag{
-    display: flex;
-    justify-content: space-between;
+.pag {
+  display: flex;
+  justify-content: space-between;
 }
 // ::v-deep .el-input__wrapper{
 // padding: 3px 11px;
 // }
 
-::v-deep .el-input__inner{
+::v-deep .el-input__inner {
   font-size: 16px;
   height: 40px;
   // border-radius: 50px;
 }
-::v-deep .el-input .el-input__icon{
+::v-deep .el-input .el-input__icon {
   position: absolute;
   right: 10px;
   top: 8px;
@@ -248,5 +280,4 @@ header{
 // .el-table{
 //   --el-table-border-color: black;
 // }
-
 </style>
