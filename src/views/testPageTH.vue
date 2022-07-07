f1ffcd7cfcf1e215e655547fcd8c92e1ed40c891
Becasuse-hung <4a33a006@stust.edu.tw>
2022-07-07 23:13:05 +0800

    okokokokok work

--- src/views/testPageTH.vue
+++ src/views/testPageTH.vue
@@ -3,18 +3,28 @@
         <div class="agent-block">
             <img src="../assets/banner.png" alt="">
             <div class="btnGroup">
-                <a href="google.com" class="hoverBtn skypeIcon"><img src="../assets/icon01.png" alt=""><div class="iconTitle">skype</div></a>
-                <a href="youtube.com" class="hoverBtn tgIcon"><img src="../assets/icon02.png" alt=""><div class="iconTitle tgTitle">合營tg</div></a>
+                <a href="google.com" class="hoverBtn skypeIcon"><img src="../assets/icon01.png" alt=""><span class="iconTitle">skype</span></a>
+                <a href="youtube.com" class="hoverBtn tgIcon"><img src="../assets/icon02.png" alt=""><span class="iconTitle tgTitle">合營tg</span></a>
                  <button class="join-us-btn">
                 <img src="../assets/btn.jpg" alt="">
                 </button>
             </div>
         </div>
-          <div class="agent-block">
+          <!-- <div class="agent-block">
             <img src="../assets/banner.png" alt="">
         </div>
           <div class="agent-block">
             <img src="../assets/banner.png" alt="">
+        </div> -->
+    </div>
+    <div class="section">
+        <div class="testBox">
+            <img src="../assets/icon01.png" alt="">
+            <span>telegram1235555</span>
+        </div>
+         <div class="testBox">
+            <img src="../assets/icon02.png" alt="">
+            <span>telegram</span>
         </div>
     </div>
 </template>
@@ -44,11 +54,15 @@ export default {
 .hoverBtn{
     background: white;
     opacity: 0.7;
-    padding: 10px;
+    padding: 15px;
     cursor: pointer;
     border-radius: 10px;
     margin-right: 30px;
-    text-decoration: none
+    text-decoration: none;
+    /*rwd center*/
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
 
 .hoverBtn:hover{
@@ -57,25 +71,36 @@ export default {
 .skypeIcon{
     width: 2vw;
 }
-.skypeIcon > a{
-    text-decoration: none;
-    padding: 20px;
+
+.tgIcon{
+    width: 2vw;
 }
+
+/*改span */
 .iconTitle{
     color: gray;
     font-size: 14px;
     word-break: keep-all;
     margin-top: 10px;
-}
-.tgIcon{
-    width: 2vw;
+
 }
 
 /* .tgtitle{
     transform: translateY(-8px);
 } */
 
-.btnGroup .join-us-btn{
+.join-us-btn{
     width: 11vw;
+}
+
+/* test */
+.testBox{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.testBox > img{
+    width: 30px;
 }
 </style>
