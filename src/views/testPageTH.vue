86e582ba9a224d6a7696e7d7248c9c72357199d8
Becasuse-hung <4a33a006@stust.edu.tw>
2022-07-13 00:53:21 +0800

    addWater

--- src/views/testPageTH.vue
+++ src/views/testPageTH.vue
@@ -1,106 +1,102 @@
-<template >
-    <div>
-        <div class="agent-block">
-            <img src="../assets/banner.png" alt="">
-            <div class="btnGroup">
-                <a href="google.com" class="hoverBtn skypeIcon"><img src="../assets/icon01.png" alt=""><span class="iconTitle">skype</span></a>
-                <a href="youtube.com" class="hoverBtn tgIcon"><img src="../assets/icon02.png" alt=""><span class="iconTitle tgTitle">合營tg</span></a>
-                 <button class="join-us-btn">
-                <img src="../assets/btn.jpg" alt="">
-                </button>
-            </div>
-        </div>
-          <!-- <div class="agent-block">
-            <img src="../assets/banner.png" alt="">
-        </div>
-          <div class="agent-block">
-            <img src="../assets/banner.png" alt="">
-        </div> -->
-    </div>
-    <div class="section">
-        <div class="testBox">
-            <img src="../assets/icon01.png" alt="">
-            <span>telegram1235555</span>
-        </div>
-         <div class="testBox">
-            <img src="../assets/icon02.png" alt="">
-            <span>telegram</span>
-        </div>
-    </div>
+<template lang="pug">
+.containerA
+  .testContent
+    img.posit(src='../assets/btn.jpg')
+    .logoA
+      img(src='../assets/btn.jpg')
+    .contentB
+      .contA
+        h2 生件
+        .infoGroup
+          .card
+            .title 累積有效獎金
+            h6 1600000
+          .card
+            .title 累積有效休假
+            h6 1600000
+      .contB
+        h2 保件
+        .card
+          .title 累積有效補修
+           h6 1600000
+  .testPosit(v-if='false')
+  img(src='../assets/btn.jpg' v-if='false')
+  .testBanner(v-if='false')
+    .testbox
+
 </template>
 <script>
 export default {
 }
 </script>
-<style scoped>
-.agent-block{
-    width: 100vw;
-    position: relative;
-    margin-bottom: 20px;
-}
-.agent-block img{
-    width: 100%;
-    display: block;
-}
-.btnGroup{
-    position: absolute;
-    top: 26.5vw;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 65%;
-}
+<style lang='sass' scoped>
+.containerA
+  border: 2px black solid
+  height: 500px
+  background: pink
+  display: flex
+  justify-content: center
+  align-items: center
+  .testContent
+    width: 35%
+    background: white
+    position: relative
+    .posit
+      position: absolute
+      width: 20%
+      top: 50%
+      left: 0
+      transform: translate(-45px, -50%) rotate(90deg)
+    .logoA
+      display: flex
+      justify-content: center
+      img
+        width: 20%
+        margin: 20px 0px
+    .contentB
+      display: flex
+      justify-content: center
+      .contA
+        // border-right: gray solid 3px
+        padding-right: 10px
+        .infoGroup
+          display: flex
+          justify-content: center
+          .card
+            margin-right: 10px
+            h6
+              margin: 20px 0px
+      .contB
+        padding-left: 10px
+        h6
+          margin: 20px 0px
+  .testPosit
+    width: 30%
+    height: 200px
+    background: yellow
+    display: flex
+    align-items: center
+    position: relative
+    z-index: 9
+//   img
+//     width: 50px
+//     height: 100px
+//     position: absolute
+//     z-index: 10
+//     left: 34%
+//     //   transform: translateX(-25px)
+    // 立體
+  .testBanner
+    width: 50%
+    height: 300px
+    background: white
+    margin: 0 auto
+    border-radius: 10px
+    .testbox
+      width: 100%
+      background: gray
+      height: 300px
+      border-radius: 10px
+      transform: translateX(4px)
 
-.hoverBtn{
-    background: white;
-    opacity: 0.7;
-    padding: 15px;
-    cursor: pointer;
-    border-radius: 10px;
-    margin-right: 30px;
-    text-decoration: none;
-    /*rwd center*/
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
-
-.hoverBtn:hover{
-    opacity: 1;
-}
-.skypeIcon{
-    width: 2vw;
-}
-
-.tgIcon{
-    width: 2vw;
-}
-
-/*改span */
-.iconTitle{
-    color: gray;
-    font-size: 14px;
-    word-break: keep-all;
-    margin-top: 10px;
-
-}
-
-/* .tgtitle{
-    transform: translateY(-8px);
-} */
-
-.join-us-btn{
-    width: 11vw;
-}
-
-/* test */
-.testBox{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
-
-.testBox > img{
-    width: 30px;
-}
 </style>
