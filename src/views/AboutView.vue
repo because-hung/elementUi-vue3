edbd977f634339cbf78c213abdc1298dbeb84115
Becasuse-hung <4a33a006@stust.edu.tw>
2025-02-22 00:48:16 +0800

    test text-overflow

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class="testBox">
+      <div class="content" v-html="content"></div>
+      <!-- <div class="content"><p>"As Chiang becomes more refined through life, the glistens of time, like fine nectar after a long period of brewing, also flow between life and art in a freer way--whether the light within his heart or</p></div> -->
+    </div>
   </div>
 </template>
+<script>
+export default {
+  name: 'aboutView',
+  setup () {
+    const content = `
+    <p>"As Chiang becomes more refined through life, the glistens of time, like fine nectar after a long period of brewing, also flow between life and art in a freer way--whether the light within his heart or</p>`
+    return {
+      content
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.testBox {
+  text-align: left;
+  .content{
+    margin: 0 auto;
+    width: 300px;
+    height: 100px;
+    border: 1px solid black;
+
+    // text-overflow
+    ::v-deep p {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 1;
+    }
+  }
+}
+</style>
